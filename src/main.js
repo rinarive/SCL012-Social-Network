68020d91ce94ee6e90299b7d7c5ed74e29fee1f3
rinarive <rinarive@gmail.com>
2020-01-24 12:39:08 -0300

    creando template de muro

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 let logo = document.getElementById("logo");
-logo.addEventListener("click",()=>{
+logo.addEventListener("click", () => {
   console.log("sirve");
-  let next=document.getElementById("root");
-  next.innerHTML= `
-  <header class="header-position">
+  let next = document.getElementById("root");
+  next.innerHTML = `
+<header class="header-position">
   <div class="marca-header">
-    <div class="encabezado"><img src="logo-weservice.png" style="cursor: pointer;" alt="logo">
+    <div class="encabezado" style="margin-left:35%"><img src="logo-weservice.png" style="cursor: pointer; width:50%" alt="logo">
+    <a class="" id="cerrar-sesion" href=""><img class="img-header" src="close.png" alt="cerrar-sesión"></a>
     </div>
   </div>
-  <div class="op-header">
-    <a class="opcion-header" href=""><img class="img-header"  src="./logo/love.png" alt="Publicaciones"></a>  
-    <a class="opcion-header" href=""><img class="img-header"  src="./logo/profiles (1).png" alt="Noticias"></a>  
-    <a class="opcion-header" href=""><img class="img-header"  src="./logo/user (1).png" alt="Perfil"></a> 
-    <a class="opcion-header" id="cerrar-sesion" href=""><img class="img-header" src="./logo/cancel.png" alt="cerrar-sesión"></a>
+
+  <div class="barra">
+    <input type="search" class="search" placeholder="Search...">
   </div>
+
 </header>
 <main>
   <div id="ingreso-post">
@@ -25,40 +25,45 @@ logo.addEventListener("click",()=>{
         <textarea  class="textarea" name="post" id="post" cols="30" rows="3" placeholder="¡Publica tus novedades!"></textarea>
       </div>
       <div class="imagen-post">
-          <select class="botones-post" name="" id="privacidad">
-              <option value="amigas">Amigas</option>
-              <option value="publico">Público</option>
-          </select>
           <button class="botones-post" id="publicar">Publicar</button>
       </div>
     </form>
   </div>
-  <section id="lista-publicaciones"></section>
+    <section id="lista-publicaciones"></section>
 </main>
-<footer class="footer-color">
-  <div class="texto-footer">
-    <div class="opciones-footer">
-        <a class="link-footer" href=""><p>INFORMACIÓN</p></a> 
-        <a class="link-footer" href=""><p>AYUDA</p></a> 
-        <a class="link-footer" href=""><p>IDIOMA</p></a> 
+<footer>
+  <footer class="footer-color">
+    <div class = "footer-opciones">
+      <a class="nav" href=""><img class="img-header"  src="like.png" alt="Publicaciones"></a>  
+      <a class="nav" href=""><img class="img-header"  src="chat-bubble.png" alt="Noticias"></a>  
+      <a class="nav" href=""><img class="img-header"  src="user.png" alt="Perfil"></a> 
     </div>
-    <div class="divisor"></div>
-    <div class="footer">
-        <img  class="footer-img" src="./logo/Sin título-1.png" alt="logo">
-        <p class="footer-texto-color"><span class="barra-navegacion">Mommy’s Love</span> Copyright © All rights reserved. </p>
-    </div>  
-  </div>
-</footer>`;
-  
-});
+  </footer>
+</footer>
+
+    `;
+    const div = document.createElement('div');
+    div.innerHTML = temp;
+    const btnLogOut = div.querySelector('#cerrar-sesion');
+    const btnPost = div.querySelector('#publicar');
+    const tagDiv = div.querySelector('#lista-publicaciones');
+    posts.forEach(post => {
+      tagDiv.appendChild(
+        cadaPost(post, firebase.auth().currentUser.uid));
+    });
+    btnPost.addEventListener('click', addPostOnSubmit);
+    btnLogOut.addEventListener('click', logOutOnClick);
+    return div;
+})
+
 
 
 let account = document.getElementById("access")
-account.addEventListener("click",()=>{
+account.addEventListener("click", () => {
   console.log("si");
-  let next=document.getElementById("root");
-  next.innerHTML= 
-  `
+  let next = document.getElementById("root");
+  next.innerHTML =
+    `
  <form class= 'formulario'>
  <input type='name' id='name' placeholder='Ingresa tu Nombre'>
  <input type='lastName' id='lastName' placeholder='Ingresa tu Apellido'>
